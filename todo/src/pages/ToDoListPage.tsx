2d0c3855edb3eaa999bc2f15366e8e8b75f05362
Yutiy Kulikov <kulikov-y@yandex.ru>
2024-10-11 21:39:28 +0300

    день 2-й финиш

---
+++ todo/src/pages/ToDoListPage.tsx
@@ -0,0 +1,14 @@
+import { Form } from "../components/Form/Form"
+import { Header } from "../components/Header/Header"
+import { ToDoList } from "../components/ToDoList/ToDolist"
+
+export const ToDoListPage = () => {
+    return (
+        <>
+            <Header />
+            <Form />
+            <ToDoList />
+        </>
+
+    )
+}
