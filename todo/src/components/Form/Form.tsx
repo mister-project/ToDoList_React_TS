95f9844deb69958f2cc8c493f94c5660310b3179
Yuriy Kulikov <kulikov-y@ya.ru>
2024-10-13 06:56:18 +0300

    29^00 в консоли печатается текст из формы

--- todo/src/components/Form/Form.tsx
+++ todo/src/components/Form/Form.tsx
@@ -1,13 +1,22 @@
+import { ChangeEvent } from 'react'
 import './Form.scss'
 export const Form = () => {
+
+let createText = ''
+
     const formSubmit = () => {
 console.log('submit')
     }
+
+const chahgeText = (event: ChangeEvent<HTMLInputElement>) => {
+console.log(event.target.value);
+}
+
     return (
         <div className="form-wrapper">
             <form action="#" onSubmit={formSubmit}>
                 <label>
-                    <input type="text" />
+                    <input type="text" onInput={chahgeText} />
                     <button></button>
                 </label>
             </form>
