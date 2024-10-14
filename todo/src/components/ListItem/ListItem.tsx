5309fb333c411a2a0fac932bf4d42feb91cac6ac
Yutiy Kulikov <kulikov-y@yandex.ru>
2024-10-14 18:08:26 +0300

    работает страница Not Found

--- todo/src/components/ListItem/ListItem.tsx
+++ todo/src/components/ListItem/ListItem.tsx
@@ -1,8 +1,12 @@
 import { ToDo } from "../../models/todo-item"
+import classes from './ListItem.module.scss'
 
 export const ListItem = ({ todo }: { todo: ToDo }) => {
     return (
-        <a href={`/list/${todo.id}`}>{todo.text}</a>
+        <a
+            className={`${classes.link} ${todo.isDone ? classes.done : classes.notDone}`}
+            href={`/list/${todo.id}`}
+        >{todo.text}</a>
 
     )
 }
