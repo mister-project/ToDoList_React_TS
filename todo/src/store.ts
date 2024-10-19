98aa4d070bdf662cb75815d8e53e000b8d8e1834
Yutiy Kulikov <kulikov-y@yandex.ru>
2024-10-19 22:19:02 +0300

    День 5 - 41

--- todo/src/store.ts
+++ todo/src/store.ts
@@ -1,17 +1,28 @@
 import { configureStore } from '@reduxjs/toolkit'
-import todoReducer from './feature/todoList'
+
+import todoReducer, { TodoState } from './feature/todoList'
+export interface RootState {
+  todoList: TodoState
+}
 
 const saveToLocalStorage = (state: RootState) => {
-const appState = JSON.stringify(state)
-localStorage.setItem('appState', appState)
+  const appState = JSON.stringify(state)
+  localStorage.setItem('appState', appState)
+}
+
+const loadFromLocalStorage = () => {
+  const appState = localStorage.getItem('appState')
+  if (!appState) return undefined
+  return JSON.parse(appState)
 }
 
 export const store = configureStore({
   reducer: {
     todoList: todoReducer
   },
+  preloadedState: loadFromLocalStorage() as RootState
 })
 
 store.subscribe(() => saveToLocalStorage(store.getState()))
-export type RootState = ReturnType<typeof store.getState>
+// export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
