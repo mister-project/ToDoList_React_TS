944082c51cd7cf331dd2fcfc612d003d17f4d962
Yuriy Kulikov <kulikov-y@ya.ru>
2024-10-20 16:02:18 +0300

    день 6 - 08^25 - появилась кнопка +

--- todo/src/components/Form/Form.tsx
+++ todo/src/components/Form/Form.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import './Form.scss'
+
 import { Button, FormBlock, Input, Label, Wrapper } from './Form.styled'
 
 import plusIcon from '../../assets/images/plus.png'
